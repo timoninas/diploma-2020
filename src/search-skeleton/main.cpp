4003b0f12dca151366da7546418a4f6c8567f569
Timonin Anton <dotruger.37@gmail.com>
2021-01-17 23:47:41 +0300

    Add code

---
+++ src/search-skeleton/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  search-skeleton
+//
+//  Created by Антон Тимонин on 17.01.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
