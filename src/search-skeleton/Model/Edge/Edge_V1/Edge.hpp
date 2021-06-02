491c60bea569a6cadcbfd48b800cccacefda372f
Timonin Anton <dotruger.37@gmail.com>
2021-06-02 22:46:04 +0300

    DIPLOM Рефактор кода

--- src/search-skeleton/Model/Edge/Edge_V1/Edge.hpp
+++ src/search-skeleton/Model/Edge/Edge_V1/Edge.hpp
@@ -21,11 +21,21 @@ typedef struct edge {
     std::pair<int, int> numberVertices;
     GraphLabels label;
     edge() = delete;
-    edge(int number, point_t p1, point_t p2, int numberVertex1, int numberVertex2):
-                                            numberEdge(number),
-                                            points(std::make_pair(p1, p2)),
-                                            numberVertices(std::make_pair(numberVertex1, numberVertex2)),
-                                            label(GraphLabels::notvisited) { }
+    edge(int number, point_t p1, point_t p2, int numberVertex1, int numberVertex2): numberEdge(number), points(std::make_pair(p1, p2)), numberVertices(std::make_pair(numberVertex1, numberVertex2)), label(GraphLabels::notvisited) { }
+    
+    edge_v2_t adaptToEdgeV2() {
+        edge_v2_t tmpEdge{numberEdge, numberVertices.first, numberVertices.second};
+        return tmpEdge;
+    }
+    
+    void swipeVertices() {
+        auto tmp = numberVertices.first;
+        numberVertices.first = numberVertices.second;
+        numberVertices.second = tmp;
+        auto tmpp = points.first;
+        points.first = points.second;
+        points.second = tmpp;
+    }
     
     bool isNotvisited() {
         return label == GraphLabels::notvisited;
@@ -37,21 +47,6 @@ typedef struct edge {
     
     bool isInskeleton() {
         return label == GraphLabels::inskeleton;
-    }
-    
-    edge_v2_t adaptToEdgeV2() {
-        edge_v2_t tmpEdge{numberEdge, numberVertices.first, numberVertices.second};
-        return tmpEdge;
-    }
-    
-    void swipeVertices() {
-        auto tmp = numberVertices.first;
-        numberVertices.first = numberVertices.second;
-        numberVertices.second = tmp;
-        
-        auto tmpp = points.first;
-        points.first = points.second;
-        points.second = tmpp;
     }
 } edge_t;
 
